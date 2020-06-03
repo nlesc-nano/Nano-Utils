8d2491e2834b85625e151041c7287a0d2494040b
Bas van Beek <b.f.van.beek@vu.nl>
2020-06-03 16:24:56 +0200

    Updated tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,10 @@
 """Tests for :mod:`nanoutils.utils`."""
 
+from inspect import isclass
+from functools import partial
+
 from assertionlib import assertion
-from nanoutils import set_docstring
+from nanoutils import set_docstring, get_importable, VersionInfo, version_info
 
 
 def test_set_docstring() -> None:
@@ -17,3 +20,34 @@ def test_set_docstring() -> None:
 
     assertion.eq(func1.__doc__, 'TEST')
     assertion.eq(func2.__doc__, None)
+
+
+def test_get_importable() -> None:
+    """Tests for :func:`nanoutils.get_importable`."""
+    class Test:
+        split = True
+
+    assertion.is_(get_importable('builtins.dict'), dict)
+    assertion.is_(get_importable('builtins.dict', validate=isclass), dict)
+
+    assertion.assert_(get_importable, None, exception=TypeError)
+    assertion.assert_(get_importable, Test, exception=TypeError)
+    assertion.assert_(get_importable, 'builtins.len', validate=isclass, exception=RuntimeError)
+    assertion.assert_(get_importable, 'builtins.len', validate=partial(isclass),
+                      exception=RuntimeError)
+
+
+def test_version_info() -> None:
+    """Tests for :func:`nanoutils.VersionInfo`."""
+    tup1 = VersionInfo(0, 1, 2)
+    tup2 = VersionInfo.from_str('0.1.2')
+    assertion.eq(tup1, (0, 1, 2))
+    assertion.eq(tup2, (0, 1, 2))
+
+    assertion.eq(tup1.micro, tup1.patch)
+
+    assertion.assert_(VersionInfo.from_str, b'0.1.2', exception=TypeError)
+    assertion.assert_(VersionInfo.from_str, '0.1.2a', exception=ValueError)
+    assertion.assert_(VersionInfo.from_str, '0.1.2.3.4', exception=ValueError)
+
+    assertion.isinstance(version_info, VersionInfo)
