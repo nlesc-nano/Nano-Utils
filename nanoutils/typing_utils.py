30d9e1df6d03b61f7b73b4f30348c462dc4ba984
Bas van Beek <b.f.van.beek@vu.nl>
2020-06-03 13:43:50 +0200

    Added `SupportsIndex`, `TypedDict` and `runtime_checkable`

--- nanoutils/typing_utils.py
+++ nanoutils/typing_utils.py
@@ -4,20 +4,37 @@ Contains aliases for ``python >= 3.8`` exclusive objects related to typing.
 
 Index
 -----
-========================= ===================================================================
-:data:`~typing.Literal`   Special typing form to define literal types (a.k.a. value types).
-:data:`~typing.Final`     Special typing construct to indicate final names to type checkers.
-:func:`~typing.final`     A decorator to indicate final methods and final classes.
-:class:`~typing.Protocol` Base class for protocol classes.
-========================= ===================================================================
+=================================== ======================================================================================================================================
+:data:`~typing.Literal`             Special typing form to define literal types (a.k.a. value types).
+:data:`~typing.Final`               Special typing construct to indicate final names to type checkers.
+:func:`~typing.final`               A decorator to indicate final methods and final classes.
+:class:`~typing.Protocol`           Base class for protocol classes.
+:class:`~typing.SupportsIndex`      An ABC with one abstract method :meth:`~SupportsIndex.__index__`.
+:class:`~typing.TypedDict`          A simple typed name space. At runtime it is equivalent to a plain :class:`dict`.
+:func:`~typing.runtime_checkable`   Mark a protocol class as a runtime protocol, so that it an be used with :func:`isinstance()` and :func:`issubclass()`.
+=================================== ======================================================================================================================================
 
-"""
+"""  # noqa: E501
 
 import sys
+from abc import abstractmethod
 
 if sys.version_info < (3, 8):
-    from typing_extensions import Literal, Final, final, Protocol
-else:
-    from typing import Literal, Final, final, Protocol
+    from typing_extensions import Literal, Final, final, Protocol, TypedDict, runtime_checkable
 
-__all__ = ['Literal', 'Final', 'final', 'Protocol']
+    @runtime_checkable
+    class SupportsIndex(Protocol):
+        """An ABC with one abstract method :meth:`__index__`."""
+
+        __slots__ = ()  # type: ignore
+
+        @abstractmethod
+        def __index__(self) -> int:
+            pass
+
+else:
+    from typing import Literal, Final, final, Protocol, TypedDict, SupportsIndex, runtime_checkable
+
+__all__ = [
+    'Literal', 'Final', 'final', 'Protocol', 'SupportsIndex', 'TypedDict', 'runtime_checkable'
+]
