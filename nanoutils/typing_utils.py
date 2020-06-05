e121fa2a2435fdeda958419ebf2b3b4205940846
Bas van Beek <b.f.van.beek@vu.nl>
2020-06-05 21:17:36 +0200

    Update typing_utils.py

--- nanoutils/typing_utils.py
+++ nanoutils/typing_utils.py
@@ -66,22 +66,3 @@ __all__ = [
 
 PathType = Union[str, bytes, os.PathLike]
 
-OpenTextMode = Literal[
-    'r', 'r+', '+r', 'rt', 'tr', 'rt+', 'r+t', '+rt', 'tr+', 't+r', '+tr',
-    'w', 'w+', '+w', 'wt', 'tw', 'wt+', 'w+t', '+wt', 'tw+', 't+w', '+tw',
-    'a', 'a+', '+a', 'at', 'ta', 'at+', 'a+t', '+at', 'ta+', 't+a', '+ta',
-    'x', 'x+', '+x', 'xt', 'tx', 'xt+', 'x+t', '+xt', 'tx+', 't+x', '+tx',
-    'U', 'rU', 'Ur', 'rtU', 'rUt', 'Urt', 'trU', 'tUr', 'Utr',
-]
-
-OpenBinaryMode = Literal[
-    'rb+', 'r+b', '+rb', 'br+', 'b+r', '+br',
-    'wb+', 'w+b', '+wb', 'bw+', 'b+w', '+bw',
-    'ab+', 'a+b', '+ab', 'ba+', 'b+a', '+ba',
-    'xb+', 'x+b', '+xb', 'bx+', 'b+x', '+bx',
-    'wb', 'bw',
-    'ab', 'ba',
-    'xb', 'bx',
-    'rb', 'br',
-    'rbU', 'rUb', 'Urb', 'brU', 'bUr', 'Ubr',
-]
