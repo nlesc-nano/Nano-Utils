88d2349ffe45c379669d2476902d7afc1c178d3c
Bas van Beek <b.f.van.beek@vu.nl>
2020-06-07 19:00:51 +0200

    Enabled the builtin `sphinx.ext.duration` extension

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,8 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.autosummary',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx.ext.duration'
 ]
 
 
