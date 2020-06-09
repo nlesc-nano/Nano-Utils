1e8de50503fd256739755f4fa5875a1378bcfb44
Bas van Beek <b.f.van.beek@vu.nl>
2020-06-09 20:02:05 +0200

    Decorate one of the tests with `@delete_finally()`

--- tests/test_testing_utils.py
+++ tests/test_testing_utils.py
@@ -8,28 +8,32 @@ from nanoutils import FileNotFoundWarning, delete_finally
 
 PATH = Path('tests') / 'test_files'
 
+DIR1 = PATH / 'tmp_dir'
+FILE1 = PATH / 'tmp_file.txt'
+
 
 def test_warning() -> None:
     """Test :exc:`nanoutils.FileNotFoundWarning`."""
     assertion.issubclass(FileNotFoundWarning, ResourceWarning)
 
 
+@delete_finally(DIR1, FILE1, warn=False)
 def test_delete_finally() -> None:
     """Test :exc:`nanoutils.FileNotFoundWarning`."""
-    dir1 = PATH / 'tmp_dir'
-    file1 = PATH / 'tmp_file.txt'
+    assertion.isfile(FILE1, invert=True)
+    assertion.isdir(DIR1, invert=True)
 
-    os.mkdir(dir1)
-    with open(file1, 'w'):
+    os.mkdir(DIR1)
+    with open(FILE1, 'w'):
         pass
 
-    assertion.isfile(file1)
-    assertion.isdir(dir1)
+    assertion.isfile(FILE1)
+    assertion.isdir(DIR1)
 
-    @delete_finally(dir1, file1)
+    @delete_finally(DIR1, FILE1)
     def func() -> None:
         pass
 
     func()
-    assertion.isfile(file1, invert=True)
-    assertion.isdir(dir1, invert=True)
+    assertion.isfile(FILE1, invert=True)
+    assertion.isdir(DIR1, invert=True)
