1376affbc5377f82795da89421684ed779929c85
Bas van Beek <b.f.van.beek@vu.nl>
2020-06-12 21:24:29 +0200

    Updated tests

--- tests/test_sphinx.py
+++ tests/test_sphinx.py
@@ -1,20 +1,18 @@
 """Test the :mod:`sphinx` documentation generation."""
 
-import shutil
-from os.path import join, isdir
+from os.path import join
 from sphinx.application import Sphinx
+
+from nanoutils import delete_finally
 
 SRCDIR = CONFDIR = 'docs'
 OUTDIR = join('tests', 'test_files', 'build')
 DOCTREEDIR = join('tests', 'test_files', 'build', 'doctrees')
 
 
+@delete_finally(OUTDIR)
 def test_sphinx_build() -> None:
     """Test :meth:`sphinx.application.Sphinx.build`."""
     app = Sphinx(SRCDIR, CONFDIR, OUTDIR, DOCTREEDIR,
                  buildername='html', warningiserror=True)
-    try:
-        app.build(force_all=True)
-    finally:
-        if isdir(OUTDIR):
-            shutil.rmtree(OUTDIR)
+    app.build(force_all=True)
